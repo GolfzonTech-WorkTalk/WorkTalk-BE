0f847d02001bff4a0bf4b5ca35d4adbb0d8169e7
suyeon222 <qzwx604@naver.com>
2022-12-22 15:12:30 +0900

    스웨거 설명 추가

--- src/main/java/com/golfzonTech4/worktalk/controller/MainController.java
+++ src/main/java/com/golfzonTech4/worktalk/controller/MainController.java
@@ -3,6 +3,11 @@ package com.golfzonTech4.worktalk.controller;
 import com.golfzonTech4.worktalk.dto.space.SpaceSearchDto;
 import com.golfzonTech4.worktalk.repository.ListResult;
 import com.golfzonTech4.worktalk.service.SpaceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
@@ -18,6 +23,14 @@ public class MainController {
     private final SpaceService spaceService;
 
     //메인페이지 - 전체 사무공간 목록, (오피스,데스크,회의실) 필터기능, 사무공간명, 주소 키워드 검색
+    @Operation(summary = "메인페이지", description = "메인페이지의 사무공간 리스트를 출력합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(schema = @Schema(implementation = ListResult.class))),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
     @GetMapping(value = "/main")
     public ResponseEntity<ListResult> mainPage(@ModelAttribute SpaceSearchDto dto) {
         log.info("SpaceSearchDto :{}", dto);
